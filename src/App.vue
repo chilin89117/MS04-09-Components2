67da0cf4023169bcddc0522d04ff562c1132af49
Chi Lin <chilin89117@gmail.com>
2017-10-10 00:46:00 -0700

    Up to lecture 124

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <appQuote>
+          <!-- Split data into 2 named slots -->
+          <!-- Default: elements with no slots go to un-named <slot> -->
+          <h3 slot="title">{{quoteTitle}}</h3>
+          <p slot="content">{{quote}}</p>
+        </appQuote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue'
+
+  export default {
+    data() {
+      return {
+        quoteTitle: 'The Quote',
+        quote: 'To be or not to be...',
+      };
+    },
+    components: {
+      appQuote: Quote,
+    },
+  };
+</script>
+
+<style scoped>
+  h3 {
+    color: blue;
+  }
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="title">
+      <slot name="title"></slot>
+      <span><slot name="subtitle">The Subtitle</slot></span>
+    </div>
+    <hr>
+    <div class="quote">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  };
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #aaa;
+    box-shadow: 2px 2px 5px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+
+  /* Styling in the child component is applied to data received in <slot> */
+  h3 {
+    color: red;
+  }
+
+  .title {
+    font-style: italic;
+  }
+</style>
